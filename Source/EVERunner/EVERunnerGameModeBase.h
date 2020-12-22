23f04e84bb81b66670ea545f972f244b48dd031f
Davikson <davikson1@hotmail.com>
2020-12-22 20:49:45 +0100

    Create blank project

---
+++ Source/EVERunner/EVERunnerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EVERunnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVERUNNER_API AEVERunnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
